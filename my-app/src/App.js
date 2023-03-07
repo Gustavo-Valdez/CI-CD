0006677f72ef7daff530912e2e6d62835f82b946
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2023-03-07 14:09:38 -0600

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prueba <code> 2 </code>
+          Prueba <code> 3 </code>
         </p>
         <a
           className="App-link"
